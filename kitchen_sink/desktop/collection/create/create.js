a11916163d5c704f402c27e7a98a9e1d368f18b9
rhiokim <rhio.kim@gmail.com>
2013-01-15 17:30:20 +0900

    append usergrid sdk 0.10.3

---
+++ kitchen_sink/desktop/collection/create/create.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	var client = new baasio.Client({
+    orgName:'6666a80e-31ed-11e2-a2c1-02003a570010', //your orgname goes here (not case sensitive)
+    appName:'e9fcb221-384f-11e2-a2c1-02003a570010', //your appname goes here (not case sensitive)
+    logging: false, //optional - turn on logging, off by default
+    buildCurl: false //optional - turn on curl commands, off by default
+  });
+
+
+});
