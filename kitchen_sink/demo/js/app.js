21a86b80bcf1fca1b286e1c3b119f93701883630
rhiokim <rhio.kim@gmail.com>
2013-01-29 18:41:37 +0900

    task signup process in demo app

--- kitchen_sink/demo/js/app.js
+++ kitchen_sink/demo/js/app.js
@@ -1,16 +1,14 @@
+var client = new Baas.IO({
+  orgName: window.YOUR_BAAS_IO_ID, //baas.io ID
+  appName: window.YOUR_BAAS_APP_ID, //Application ID
+  logging: true,
+  buildCurl: true
+});
+
 $(document).ready(function () {
 
 	var appUser;
 	var myCollection;
-
-	var client = new Baas.IO({
-		//baas.io ID
-		orgName: 'YOUR_BAAS_IO_ID',
-		//Application ID
-		appName: 'YOUR_APPLICATION_ID',
-		logging: true,
-		buildCurl: true
-	});
 
 	function isLoggedIn() {
     if (!client.isLoggedIn()) {
@@ -36,7 +34,6 @@ $(document).ready(function () {
             } else {
               if (client.isLoggedIn()){
                 appUser = user;
-               // showFullFeed();
               }
             }
           });
@@ -44,98 +41,10 @@ $(document).ready(function () {
           //clear out the login form so it is empty if the user chooses to log out
           $("#username").val('');
           $("#password").val('');
-
-          //default to the full feed view (all messages in the system)
-          showMyCollection();
         }
       });
   }
 
-  function drawItems(collection) {
-  	var html = '';
-
-  	while(collection.hasNextEntity()) {
-      var entity = collection.getNextEntity(),
-		      created = entity.get('created'),
-		      name = entity.get('name'),
-		      price = entity.get('price');
-
-		  html += '<li><a href="acura.html">'+ name +'</a></li>';
-  	}
-
-    if (html == "") { html = "No Entitiy"; }
-    $("#entities").html(html).listview('refresh');
-
-    //next show the next / previous buttons
-    if (collection.hasPreviousPage()) {
-      $("#previous-btn-container").show();
-    } else {
-      $("#previous-btn-container").hide();
-    }
-    if (collection.hasNextPage()) {
-      $("#next-btn-container").show();
-    } else {
-      $("#next-btn-container").hide();
-    }
-  }
-
-  function prettyDate(createdDateValue) {
-    var diff = (((new Date()).getTime() - createdDateValue) / 1000)
-    var day_diff = Math.floor(diff / 86400);
-
-    if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
-      return 'just now';
-
-    return fred = day_diff == 0 && (
-      diff < 60 && "just now" ||
-      diff < 120 && "1 minute ago" ||
-      diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-      diff < 7200 && "1 hour ago" ||
-      diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-      day_diff == 1 && "Yesterday" ||
-      day_diff < 7 && day_diff + " days ago" ||
-      day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
-  }
-
-  function showMyCollection() {
-    if (!isLoggedIn()) return;
-
-    //make sure we are on the messages page
-    window.location = "#page-collection";
-
-    // fullFeedView = false;
-    // $('#btn-show-full-feed').removeClass('ui-btn-up-c');
-    // $('#btn-show-my-feed').addClass('ui-btn-up-c');
-
-    if (myCollection) {
-      myCollection.resetPaging();
-      myCollection.fetch(function (err) {
-        if (err) {
-          alert('Could not get user feed. Please try again.');
-        } else {
-        	console.log(JSON.stringify(myCollection, null, 2));
-          drawItems(myCollection);
-        }
-      });
-    } else {
-      // 컬렉션이 없는 경우
-      var options = {
-        // type:'user/me/feed',
-        type: 'mycollections',
-        qs: {"ql": "order by created desc"}
-      }
-      client.createCollection(options, function(err, collectionObj){
-        if (err) {
-         alert('Could not get user feed. Please try again.');
-        } else {
-          myCollection = collectionObj;
-        	console.log(JSON.stringify(myCollection, null, 2));
-          drawItems(myCollection);
-        }
-      });
-    }
-  }
-
-	$('#btn-login').bind('click', login);
+  $('#btn-login').bind('click', login);
 
 });
